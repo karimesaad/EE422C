de3e5276fbec3a28621221e8212e9da37e276dbb
Karime Saad <karime_saad@utexas.edu>
2016-02-03 20:38:56 -0600

    Added multiply.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print(add(2,3));
+	}
 
+	static int add(int x, int y){
+		int sum = 0;
+		for(int i = 0; i < x; i++){
+			sum += y;
+		}
+		return sum;
 	}
 
 }
